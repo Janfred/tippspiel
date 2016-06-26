d29c446e8580860ee58002be884c9e4bc950e803
Jan-Frederik Rieckers <rieckers@uni-bremen.de>
2016-06-26 17:46:04 +0200

    Fix username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :confirmable
 
-  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  validates_format_of :username, with: /\A[a-zA-Z0-9_\.]*\z/
 end
